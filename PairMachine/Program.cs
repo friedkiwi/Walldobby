aebd4345243bce5df195496651570d54c18036c1
Yvan Janssens <friedkiwi@yvanj.me>
2016-01-28 00:04:11 +0000

    Added new pairing tool
    Added pairing tool to pair this PC with the new LightwaveRF WiFi Link
    software version

---
+++ PairMachine/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LightwaveRF.API.sendRaw(":202D4D,100,!F*p", 0);
+            LightwaveRF.API.sendRaw(":202D4D,100,!F*p", 0);
+            LightwaveRF.API.sendRaw(":202D4D,100,!F*p", 0);
+        }
+    }
+}

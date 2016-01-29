54fc0e72ba1967e879cc89bd13783b9dc1467cea
Yvan Janssens <friedkiwi@yvanj.me>
2016-01-29 06:27:07 +0000

    Added VirtualUI code path

--- Walldobby/Program.cs
+++ Walldobby/Program.cs
@@ -14,6 +14,7 @@ namespace Walldobby
         [STAThread]
         static void Main()
         {
+            new Cybele.Thinfinity.VirtualUI().Start();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
